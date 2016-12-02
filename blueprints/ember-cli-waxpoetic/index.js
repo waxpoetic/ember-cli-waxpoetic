c6e14d57474ba38964a862bf0786b07a7517fad7
Tom Scott <tscott@weblinc.com>
2016-12-01 19:17:40 -0500

    Install addons

--- blueprints/ember-cli-waxpoetic/index.js
+++ blueprints/ember-cli-waxpoetic/index.js
@@ -9,6 +9,10 @@ module.exports = {
   //   };
   // }
 
+
   afterInstall: function(options) {
+    this.addBowerPackageToProject('bourbon', '4.2.7');
+    this.addBowerPackageToProject('neat', '1.8.0');
+    require('child_process').spawn('bitters install --path=app/styles');
   }
 };
--- config/environment.js
+++ config/environment.js
@@ -5,7 +5,7 @@ module.exports = function(environment, ENV) {
   if (environment === 'production') {
     ENV.APP.host = 'https://api.waxpoeticrecords.com';
   } else {
-    ENV.APP.host = 'http://localhost:3000';
+    ENV.APP.host = 'http://waxpoetic.docker';
   }
 
   return ENV;
