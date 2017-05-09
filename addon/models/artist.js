ad11967c2365d7d9f6a1842db4f6d62b2d17d02f
Tom Scott <tscott@weblinc.com>
2017-05-09 18:53:26 -0400

    Add youtubePlaylistId and startingYoutubeVideoId to artists

--- addon/models/artist.js
+++ addon/models/artist.js
@@ -4,5 +4,7 @@ export default DS.Model.extend({
   name: DS.attr('string'),
   bio: DS.attr('string'),
   photo: DS.attr('string'),
-  releases: DS.hasMany('release')
+  releases: DS.hasMany('release'),
+  youtubePlaylistId: DS.attr('string'),
+  startingYoutubeVideoId: DS.attr('string')
 });
